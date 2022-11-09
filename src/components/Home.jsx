802b99c8415f6930eb40fd4cb4426514e51c8041
Violet's Foxes <111033412+VioletsFoxes@users.noreply.github.com>
2022-11-09 22:44:27 +0800

    update style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,12 @@ function Main() {
         <section className="Main">
             <div className="Describe">
                 <h2 className='Describe--Text'>
-                    Hello! De-Sheng Chen, study in NYUST, a{" "}
+                    Hello! De-Sheng Chen, study in 
+                    <span style={{
+                        background: "linear-gradient(45deg, #08AEEA 0%, #2AF598 100%)",
+                        WebkitBackgroundClip: 'text',
+                        WebkitTextFillColor: 'transparent',
+                    }}>NYUST</span>, a{" "}
                     <span style={{
                         background: "-webkit-linear-gradient(225deg, #FAD961 0%, #F76B1C 100%)",
                         WebkitBackgroundClip: "text",
