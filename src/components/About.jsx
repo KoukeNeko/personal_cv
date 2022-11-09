c8c38618972f9b25303d1c756f2dfb04cec35325
Violet's Foxes <111033412+VioletsFoxes@users.noreply.github.com>
2022-11-09 23:39:21 +0800

    create About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default function About() {
   return (
     <section className="About">
+        <div className="About--Profile">
+            <img src="https://scontent.frmq2-2.fna.fbcdn.net/v/t1.6435-9/123136908_170188244732123_1634315602398268329_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=TxHMC2FC6W0AX_q2ct3&tn=wfIykJdd3abDQbvB&_nc_ht=scontent.frmq2-2.fna&oh=00_AfBBuvdKMNaHJjteVVpYfy0INWqvecoE4O-u3aI52t18qA&oe=63927A51" alt="Profile" />
+            <h2 className='About--Name'>陳德生</h2>
+        </div>
+        <div className="Profile--Details">
         
+            <h2 className='About--Name'>陳德生</h2>
+        </div>
     </section>
   )
 }
